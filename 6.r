00d419408eb154e3110b927c15f2d1b2951da71b
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-27 11:53:17 +0530

    Create 6.r

---
+++ 6.r
@@ -0,0 +1,30 @@
+df <- data.frame(
+    col1 = c(1, 2, 3, 4),
+    col2 = c("a", "b", "c", "d"),
+    col3 = c(5, 6, 7, 8)
+)
+
+# Print the original data frame
+print(df);
+
+replace_with_na <- function(data_frame, value) {
+    # Loop through each column of the data frame
+    for (col in colnames(data_frame)) {
+    # Replace the specified value with NA in the column
+    data_frame[[col]][data_frame[[col]] == value] <- NA
+    }
+    return(data_frame)
+}
+
+# Create a sample data frame
+df <- data.frame(
+    col1 = c(1, 2, 3, 4),
+    col2 = c("a", "b", "c", "d"),
+    col3 = c(5, 6, 7, 8)
+)
+# Print the original data frame
+print(df)
+# Call the replace_with_na function to replace "b" with NA
+df_new <- replace_with_na(df, "b")
+# Print the modified data frame
+print(df_new)
