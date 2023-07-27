1f5ea0597fbc8178ab212d252d656de8efd356f9
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-27 11:52:45 +0530

    Create 4.r

---
+++ 4.r
@@ -0,0 +1,20 @@
+# a.
+for (a in 1:3) { 
+    for (b in 1:3) {
+        print(paste("a =", a, "b =", b))
+        if (b == 2) {
+            break
+        } 
+    }
+}
+
+# b. 
+i <- 2
+while (i <= 5) { 
+    if (i == 3) { 
+        i <- i+1
+        next
+    }
+    print(i)
+    i <- i + 1 
+}
